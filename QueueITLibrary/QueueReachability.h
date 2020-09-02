ae9ed66e39a70d4f245e0d0668a971a1c195e4bb
LuLuGaGa <LuLuGaGa@users.noreply.github.com>
2020-09-02 09:40:39 +0100

    Update QueueReachability.h
    rename rename kReachabilityChangedNotification

--- QueueITLibrary/QueueReachability.h
+++ QueueITLibrary/QueueReachability.h
@@ -57,7 +57,7 @@ typedef enum : NSInteger {
 } NetworkStatus;
 
 
-extern NSString *kReachabilityChangedNotification;
+extern NSString *ReachabilityChangedNotification;
 
 
 @interface QueueReachability : NSObject
